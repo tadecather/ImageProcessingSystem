0c50725c5c5eec364aefe968cb8bd0620cf90d0f
mecese <1948743076@qq.com>
2017-08-15 09:12:11 +0800

    transformation模块

---
+++ src/ImageProcessingSystem/imagetrans.cpp
@@ -0,0 +1,6 @@
+#include "imagetrans.h"
+
+imagetrans::imagetrans()
+{
+
+}
---
+++ src/ImageProcessingSystem/imagetrans.h
@@ -0,0 +1,11 @@
+#ifndef IMAGETRANS_H
+#define IMAGETRANS_H
+
+
+class imagetrans
+{
+public:
+    imagetrans();
+};
+
+#endif // IMAGETRANS_H
